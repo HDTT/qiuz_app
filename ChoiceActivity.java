126cef7b25deac083f32a013addc2f97951050bb
DatNguyen0708 <ndat905@gmail.com>
2017-04-20 00:27:40 +0700

    QuizApp
    Úp giao diện phần main và các thể loại câu hỏi lựa chọn sau đó hiển thị các câu hỏi để bắt đầu trò chơi

---
+++ ChoiceActivity.java
@@ -0,0 +1,104 @@
+﻿import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ChoiceActivity extends AppCompatActivity {
+    private List<Type> list;
+    private MediaPlayer mediaPlayer;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choice);
+
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+
+        list = new ArrayList();
+        list.add(new Type("Khoa Học Xã hội"));
+        list.add(new Type("Toán Học"));
+        list.add(new Type("Văn Học - Lịch Sử"));
+        list.add(new Type("Địa Lý"));
+        list.add(new Type("Câu Đố Vui"));
+        list.add(new Type("Câu Đố Mẹo"));
+        list.add(new Type("Câu Đố Hay"));
+        list.add(new Type("Câu Đố Cười"));
+
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rcv);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(new RecyclerDataAdapter(this, list));
+
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.nhacnen);
+//        mediaPlayer.setLooping(true);
+//        mediaPlayer.start();
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+               // onDestroy();
+                onBackPressed();
+
+
+               // mediaPlayer.setVolume(0,0);
+                //mediaPlayer.setVolume(1,1);
+                break;
+            default:
+               // return super.onOptionsItemSelected(item);
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onBackPressed(){
+//        mediaPlayer.stop();
+//        mediaPlayer.reset();
+//        mediaPlayer.release();
+//        mediaPlayer = null;
+//        if(mediaPlayer.isPlaying())
+//            mediaPlayer.stop();
+//
+//        mediaPlayer.release();
+    //    Intent myIntent=new Intent(this, MainActivity.class);
+      //  startActivity(myIntent);
+        finish();
+    }
+  //  @Override
+    //protected void onDestroy() {
+
+//        super.onDestroy();
+//        Intent myIntent=new Intent(this, MainActivity.class);
+//        startActivity(myIntent);
+//       // mediaPlayer.stop();
+//        mediaPlayer.reset();
+//        mediaPlayer.release();
+//        mediaPlayer = null;
+      //  finish();
+
+   // }
+
+}
---
+++ RecyclerDataAdapter.java
@@ -0,0 +1,111 @@
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+
+public class RecyclerDataAdapter extends RecyclerView.Adapter<RecyclerDataAdapter.DataViewHolder> {
+
+    private List<Type> name;
+    private Context context;
+    private LayoutInflater mInflater;
+    private RecyclerDataAdapter mAdapter;
+
+    public RecyclerDataAdapter(Context context, List<Type> name) {
+        this.context = context;
+        this.name = name;
+        this.mInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getItemCount() {
+        return name.size();
+     //   return name == null ? 0 : name.size();
+    }
+
+    @Override
+    public RecyclerDataAdapter.DataViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView;
+
+        itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row, parent, false);
+
+
+        return new DataViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerDataAdapter.DataViewHolder holder, int position) {
+       // String name1 = name.get(position).getName();
+        //holder.tvName.setText(name1);
+        Type type = name.get(position);
+        holder.tvName.setText(type.getName());
+    }
+
+    /**
+     * Data ViewHolder class.
+     */
+  //  public class DataViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public class DataViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    private TextView tvName;
+        private MediaPlayer mediaPlayer;
+        private  Context context;
+        private Intent intent;
+
+        ChoiceActivity choice = new ChoiceActivity();
+        public DataViewHolder(View itemView) {
+            super(itemView);
+
+            itemView.setClickable(true);
+            tvName = (TextView) itemView.findViewById(R.id.tvName);
+            itemView.setOnClickListener(this);
+            context = itemView.getContext();
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position  =   getAdapterPosition();
+            Intent intent = new Intent(context,QuestionActivity.class);
+            Bundle b = new Bundle();
+            b.putInt("position", position); // Your score
+            intent.putExtras(b); // Put your score to your next
+            context.startActivity(intent);
+            Toast.makeText(context, String.valueOf(getAdapterPosition()), Toast.LENGTH_SHORT).show();
+
+
+
+            Intent intent1 = new Intent(context,HighScoreActivity.class);
+            Bundle b1 = new Bundle();
+            b1.putInt("position1", position); // Your score
+            intent1.putExtras(b1);
+            //context.startActivity(intent1);// Put your score to your next
+
+
+
+        }
+
+
+//        @Override
+//        public void onClick(View v) {
+//            intent = new Intent(context,QuestionActivity.class);
+//           // mediaPlayer.pause();
+//            int position = getAdapterPosition();
+//            Bundle bundle = new Bundle();
+///           bundle.putInt("position",position);
+///           intent.putExtra("DATA",bundle);
+//            intent.putExtra("so",position);
+//            context.startActivity(intent);
+//        }
+
+    }
+
+
+
+}
